62c4f657da6d8e20a7b0c9152fe71ea4f1c80c71
moranaj2 <moranaj2@miamioh.edu>
2018-04-10 19:00:07 -0400

    using regular Image component

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import ImageViewer from 'react-native-image-zoom-viewer';
+import { StyleSheet, Text, View, TouchableOpacity,Image,Dimensions } from 'react-native';
 
 export default class Navigation extends React.Component {
 	constructor() {
@@ -22,8 +21,11 @@ export default class Navigation extends React.Component {
 					<Text>{'Use stairs:  '+this.props.stairs}</Text>
 				</View>
 				<View style={{flex:0.8}}>
-					<ImageViewer
-						imageUrls={[{source:require('../img/fsb_floor1.png')}]}
+					<Image
+						resizeMode = "contain"
+						source={floorplans.floor2}
+						style={{flex:1}}
+						width={Dimensions.get('window').width}
 					/>
 				</View>
 				<View style={{flex:0.1}}>
@@ -36,7 +38,7 @@ export default class Navigation extends React.Component {
 	}
 }
 
-const floorplans = [
-	{source: require('../img/fsb_floor1.png')},
-	{source: require('../img/fsb_floor2.png')}
-]
+const floorplans = {
+	floor1: require('../img/fsb_floor1.png'),
+	floor2: require('../img/fsb_floor2.png')
+}
