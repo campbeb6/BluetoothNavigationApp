1d7bc34d5a9acb44d543dec098b864d802336223
moranaj2 <moranaj2@miamioh.edu>
2018-04-10 17:57:43 -0400

    empty Navigation component

---
+++ components/Navigation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Navigation extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+
+		}
+	}
+	render() {
+		return(
+			<View style={{flex:1}} >
+			
+			</View>
+		);
+	}
+}
