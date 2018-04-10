efbf3e08196d0947c5a536c8cd570e0e9c507ca6
moranaj2 <moranaj2@miamioh.edu>
2018-04-10 18:20:25 -0400

    tested ImageView component, hangs on load

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import ImageView from 'react-native-image-view';
 
 export default class Navigation extends React.Component {
 	constructor() {
@@ -15,10 +16,17 @@ export default class Navigation extends React.Component {
 		// stairs
 		return(
 			<View style={{flex:1}} >
-				<View style={{flex:0.9}}>
+				<View style={{flex:0.1}}>
 					<Text>{'Starting location:  '+this.props.startingLocation}</Text>
 					<Text>{'Destination:  '+this.props.destination}</Text>
 					<Text>{'Use stairs:  '+this.props.stairs}</Text>
+				</View>
+				<View>
+					<ImageView style={{flex:0.1}}
+						images={floorplans}
+						imageIndex={0}
+						isVisible={true}
+					/>
 				</View>
 				<View style={{flex:0.1}}>
 					<TouchableOpacity onPress={this.props.goBack}>
@@ -29,3 +37,8 @@ export default class Navigation extends React.Component {
 		);
 	}
 }
+
+const floorplans = [
+	{source: require('../img/fsb_floor1.png')},
+	{source: require('../img/fsb_floor2.png')}
+]
