eeb51fc3444e17d24b083e2c84e1707845be27d1
moranaj2 <moranaj2@miamioh.edu>
2018-04-10 18:45:24 -0400

    using ImageViewer component, works for online image URLs but not for local images

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import PhotoView from 'react-native-photo-view';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 export default class Navigation extends React.Component {
 	constructor() {
@@ -21,14 +21,10 @@ export default class Navigation extends React.Component {
 					<Text>{'Destination:  '+this.props.destination}</Text>
 					<Text>{'Use stairs:  '+this.props.stairs}</Text>
 				</View>
-				<View>
-				<PhotoView
-					source={require('../img/fsb_floor1.png')}
-					minimumZoomScale={0.5}
-					maximumZoomScale={3}
-					onLoad={() => console.log("Image loaded!")}
-					style={{width: 300, height: 300}}
-				/>
+				<View style={{flex:0.8}}>
+					<ImageViewer
+						imageUrls={[{source:require('../img/fsb_floor1.png')}]}
+					/>
 				</View>
 				<View style={{flex:0.1}}>
 					<TouchableOpacity onPress={this.props.goBack}>
