d49a0f1d19b33885f605ebfdd797cfd822b42eaa
moranaj2 <moranaj2@miamioh.edu>
2018-04-10 21:09:42 -0400

    draw sample route from isa406 room to Dividends

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity,Image,Dimensions } from 'react-native';
 import PinchZoomView from 'react-native-pinch-zoom-view';
-import Svg, {Rect} from 'react-native-svg';
+import Svg, {Rect,Line} from 'react-native-svg';
 
 export default class Navigation extends React.Component {
 	constructor() {
@@ -21,9 +21,34 @@ export default class Navigation extends React.Component {
 			left: 0
 		};
 
-		// image width: 609, height: 539
+		// image dimensions (floor 1)
 		const FLOORPLAN_WIDTH = 609;
 		const FLOORPLAN_HEIGHT = 539;
+		const IMG_WIDTH = Dimensions.get('window').width;
+		const IMG_HEIGHT = Dimensions.get('window').width*(FLOORPLAN_HEIGHT/FLOORPLAN_WIDTH);
+
+		// example route
+		const route = [{
+				x: String(Math.floor(50/FLOORPLAN_WIDTH*IMG_WIDTH)),
+				y: String(Math.floor(260/FLOORPLAN_HEIGHT*IMG_HEIGHT))
+			},{
+				x: String(Math.floor(50/FLOORPLAN_WIDTH*IMG_WIDTH)),
+				y: String(Math.floor(285/FLOORPLAN_HEIGHT*IMG_HEIGHT))
+			},{
+				x: String(Math.floor(78/FLOORPLAN_WIDTH*IMG_WIDTH)),
+				y: String(Math.floor(285/FLOORPLAN_HEIGHT*IMG_HEIGHT))
+			},{
+				x: String(Math.floor(78/FLOORPLAN_WIDTH*IMG_WIDTH)),
+				y: String(Math.floor(142/FLOORPLAN_HEIGHT*IMG_HEIGHT))
+			},{
+				x: String(Math.floor(120/FLOORPLAN_WIDTH*IMG_WIDTH)),
+				y: String(Math.floor(142/FLOORPLAN_HEIGHT*IMG_HEIGHT))
+			},
+			{
+				x: String(Math.floor(120/FLOORPLAN_WIDTH*IMG_WIDTH)),
+				y: String(Math.floor(105/FLOORPLAN_HEIGHT*IMG_HEIGHT))
+			}
+		];
 		return(
 			<View style={{flex:1}} >
 				<View style={{flex:0.1}}>
@@ -46,17 +71,65 @@ export default class Navigation extends React.Component {
 						<Rect
 							x="0"
 							y="0"
-							width="70"
-							height="70"
+							width={Dimensions.get('window').width}
+							height={Dimensions.get('window').width*(FLOORPLAN_HEIGHT/FLOORPLAN_WIDTH)}
 							stroke="red"
 							strokeWidth="2"
 							fill="none"
 						/>
+						<Line
+							x1={route[0].x}
+							y1={route[0].y}
+							x2={route[1].x}
+							y2={route[1].y}
+							stroke="red"
+							strokeWidth="2"
+						/>
+						<Line
+							x1={route[1].x}
+							y1={route[1].y}
+							x2={route[2].x}
+							y2={route[2].y}
+							stroke="red"
+							strokeWidth="2"
+						/>
+						<Line
+							x1={route[2].x}
+							y1={route[2].y}
+							x2={route[3].x}
+							y2={route[3].y}
+							stroke="red"
+							strokeWidth="2"
+						/>
+						<Line
+							x1={route[3].x}
+							y1={route[3].y}
+							x2={route[4].x}
+							y2={route[4].y}
+							stroke="red"
+							strokeWidth="2"
+						/>
+						<Line
+							x1={route[4].x}
+							y1={route[4].y}
+							x2={route[5].x}
+							y2={route[5].y}
+							stroke="red"
+							strokeWidth="2"
+						/>
 					</Svg>
 				</PinchZoomView>
-				<View style={{flex:0.1}}>
+				<View style={{
+					flex:0.1,
+					backgroundColor: '#000000',
+					alignItems: 'center'
+				}}>
 					<TouchableOpacity onPress={this.props.goBack}>
-						<Text>{'BACK'}</Text>
+						<Text style={{
+							color:'#FFFFFF',
+							fontSize: 20,
+							fontWeight: 'bold'
+						}}>{'BACK'}</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
