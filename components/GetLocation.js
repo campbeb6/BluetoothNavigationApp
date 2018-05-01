72a8c00ee5c2f32e787fe18e04455791dc34d159
moranaj2 <moranaj2@miamioh.edu>
2018-05-01 12:38:34 -0400

    remove call and import for AndroidBeacon. native code crashes as soon as app runs

--- components/GetLocation.js
+++ components/GetLocation.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import AndroidBeacon from './Beacon';
+// import AndroidBeacon from './Beacon';
 
 export default class GetLocation extends React.Component {
 	constructor() {
 		super();
 		console.log('GetLocation constructor');
-		// call Android native module
-		AndroidBeacon.test(function(res){
-			console.log('GetLocation: '+res);
-		});
 	}
 	render() {
 		return(
