e586671a8321e55dea389b6ba8df5d6955d5b14e
moranaj2 <moranaj2@miamioh.edu>
2018-05-06 15:50:59 -0400

    import Platform. ignore beacons 1000 and 2000

--- components/RCTBeaconManager.js
+++ components/RCTBeaconManager.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, DeviceEventEmitter } from 'react-native';
+import { StyleSheet, Text, View, DeviceEventEmitter, Platform } from 'react-native';
 import Beacons from 'react-native-beacons-manager';
 
 // https://github.com/MacKentoch/react-native-beacons-manager/tree/master/examples/samples#detailed-documentation--sample-code
@@ -41,7 +41,8 @@ export default class RCTBeaconManager extends React.Component {
 		// closest beacon will have highest RSSI value (least negative)
 		for(let i=0;i<this.state.beaconsInRange.length;i++) {
 			let b = this.state.beaconsInRange[i];
-			if(b.rssi > maxRSSI) {
+			// ignore beacon 1000 and 2000 since they are not mounted
+			if(b.rssi > maxRSSI && b.minor!==1000 && b.minor!==2000) {
 				this.setState({
 					nearestBeaconMinorID: String(b.minor)
 				},()=>{
