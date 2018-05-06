94c16e6d34a22be2d3f6995471e18a5b030a9eac
moranaj2 <moranaj2@miamioh.edu>
2018-05-06 16:18:41 -0400

    rename TestEnrollments class to Enrollments

--- components/Enrollments.js
+++ components/Enrollments.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Picker, Item } from 'react-native';
 
-export default class TestEnrollments extends React.Component {
+export default class Enrollments extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             choice: ''
         }
     }
-    
+
     render() {
         if(this.props.classes.length == 0){
             return null;
